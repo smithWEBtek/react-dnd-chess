edd09f3cd8ca1b48393f137a4dd9a2ce58142fc2
brad@smithwebtek.com <brad@smithwebtek.com>
2024-04-15 09:48:01 -0400

    Basic Board with Knight

---
+++ src/Square.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Square({ black, children }) {
+  const fill = black ? 'black' : 'white'
+  const stroke = black ? 'white' : 'black'
+
+  return (
+    <div
+      style={{
+        backgroundColor: fill,
+        color: stroke,
+        width: '200px',
+        height: '80px'
+      }}
+    >
+      {children}
+    </div>
+  )
+}
